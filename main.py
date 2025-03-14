998ee86ccc28208be7a9ebeaf77fbd3abfd75971
Alekx Zarley <alekxzarley@Alekxs-Mac-mini.local>
2025-03-14 14:17:56 -0500

    turning

--- main.py
+++ main.py
@@ -2,17 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
-# from constants import *
+# import constants
+from constants import *
+from player import Player
 
 def main():
     pygame.init();
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT));
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
+    game_clock = pygame.time.Clock();
+    dt = 0;  ### delta time
+    player = Player( SCREEN_WIDTH/2, SCREEN_HEIGHT/2 );
     while( True ):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: return;
         pygame.Surface.fill( screen, (0,0,0) );
+        player.update(dt);
+        player.draw(screen);
         pygame.display.flip();
+
+        ### end
+        dt = game_clock.tick( 60 ) / 1000;
 
     # print( "Starting Asteroids!" );
     # print( f"Screen width: {constants.SCREEN_WIDTH}");     
